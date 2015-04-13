c317818c481363f9a50e0fbaf20db9075212357c
Edsel <edsel.lopez@jmaconsulting.biz>
2015-04-13 12:13:47 +0530

    CIVI-28 Added check for multiple pricesets

--- CRM/Price/BAO/PriceSet.php
+++ CRM/Price/BAO/PriceSet.php
@@ -959,6 +959,9 @@ WHERE  id = %1";
           unset($feeBlock[$key]['options'][$k]);
         }
       }
+      if (empty($feeBlock[$key]['options'])) {
+        unset($feeBlock[$key]);
+      }
     }
     // call the hook.
     CRM_Utils_Hook::buildAmount($component, $form, $feeBlock);
