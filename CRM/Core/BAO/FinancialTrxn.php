25529a53dd175f30b48a757460bfef0c6ccc7ab7
Pradeep Nayak <pradpnayak@gmail.com>
2016-08-22 23:37:37 +0530

    CRM-16189, added code to invoke hook and create entries for Online Membership
    ----------------------------------------
    * CRM-16189: Improve support for Accrual Method bookkeeping
      https://issues.civicrm.org/jira/browse/CRM-16189
    
    Conflicts:
    
    	CRM/Contribute/BAO/Contribution.php
    	CRM/Core/BAO/FinancialTrxn.php
    
    Conflicts:
    	CRM/Contribute/BAO/Contribution.php

--- CRM/Core/BAO/FinancialTrxn.php
+++ CRM/Core/BAO/FinancialTrxn.php
@@ -630,7 +630,7 @@ WHERE ft.to_financial_account_id != {$toFinancialAccount} AND ft.to_financial_ac
         'fee_amount' => '0.00',
         'currency' => $contributionDetails['currency'],
         'trxn_id' => $contributionDetails['trxn_id'],
-        'status_id' => $contributionDetails['contribution_status_id'],
+        'status_id' => array_search('Completed', $statuses),
         'payment_instrument_id' => $contributionDetails['payment_instrument_id'],
         'check_number' => $contributionDetails['check_number'],
         'is_payment' => 1,
@@ -699,6 +699,30 @@ WHERE ft.to_financial_account_id != {$toFinancialAccount} AND ft.to_financial_ac
           civicrm_api3('EntityFinancialTrxn', 'create', $entityParams);
         }
       }
+    }
+  }
+
+  /**
+   * Calculate Revenue Recognition date for Membership.
+   *
+   * @param array $params
+   *   Holds submitted formvalues and params from api for updating/adding contribution.
+   *
+   * @param int $contributionID
+   *   Contribution ID
+   *
+   */
+  public static function generateRevenueRecognitionDate(&$params, $contributionID) {
+    if (!CRM_Contribute_BAO_Contribution::checkContributeSettings('deferred_revenue_enabled')) {
+      return FALSE;
+    }
+    $membershipID = CRM_Utils_Array::value('membership_id', $params);
+    if (!$membershipID && $contributionID) {
+      $membershipID = CRM_Core_DAO::getFieldValue('CRM_Member_DAO_MembershipPayment', $contributionID, 'membership_id', 'contribution_id');
+    }
+    if ($membershipID && empty($params['revenue_recognition_date'])) {
+      $params['revenue_recognition_date'] = CRM_Utils_Array::value('receive_date', $params, date('Ymd'));
+      $params['membership_id'] = $membershipID;
     }
   }
 
