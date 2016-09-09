ebc7cfbd78083336166b358f60cdff683c0d3f47
Edsel <edsel.lopez@jmaconsulting.biz>
2016-09-09 18:15:07 +0530

    CRM-16189 AllCoreTables - Improve support for Accrual Method bookkeeping
    https://issues.civicrm.org/jira/browse/CRM-16189

--- CRM/Core/DAO/AllCoreTables.data.php
+++ CRM/Core/DAO/AllCoreTables.data.php
@@ -24,7 +24,7 @@
 | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
 +--------------------------------------------------------------------+
 */
-// (GenCodeChecksum:cca4e9b2d8c7de618449cc2fce53bc7c)
+// (GenCodeChecksum:6e933c875f2a6f455cf8f1532283550c)
 return array(
   'CRM_Core_DAO_AddressFormat' => array(
     'name' => 'AddressFormat',
