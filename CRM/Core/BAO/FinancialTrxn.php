0f8495a7af6ddfafa9723c49dde75c8b11ebbcb1
Pradeep Nayak <pradpnayak@gmail.com>
2016-10-11 23:55:17 +0530

    CRM-16189, fixed data storing in current_period_opening_balance
    ----------------------------------------
    * CRM-16189: Improve support for Accrual Method bookkeeping
      https://issues.civicrm.org/jira/browse/CRM-16189

--- CRM/Core/BAO/FinancialTrxn.php
+++ CRM/Core/BAO/FinancialTrxn.php
@@ -845,8 +845,13 @@ SUM(credit) + IF (financial_account_type_id = " . array_search('Liability', $fin
       // Update current_period_opening_balance
       $financialAccountParams = array(
         'id' => $result->civicrm_financial_account_id,
-        'current_period_opening_balance' => $result->civicrm_financial_trxn_debit - $result->civicrm_financial_trxn_credit,
       );
+      if (array_search('Asset', $financialAccountType)) {
+        $financialAccountParams['current_period_opening_balance'] = $result->civicrm_financial_trxn_debit - $result->civicrm_financial_trxn_credit;
+      }
+      else {
+        $financialAccountParams['current_period_opening_balance'] = $result->civicrm_financial_trxn_credit - $result->civicrm_financial_trxn_debit;
+      }
       civicrm_api3('FinancialAccount', 'Create', $financialAccountParams);
     }
     if (empty($rows)) {
