b7a7c8652b1e1744beb186dec61ff3569464ce65
Pradeep Nayak <pradpnayak@gmail.com>
2016-10-12 20:29:16 +0530

    CRM-16189, updated query
    ----------------------------------------
    * CRM-16189: Improve support for Accrual Method bookkeeping
      https://issues.civicrm.org/jira/browse/CRM-16189

--- CRM/Report/Form/Contribute/TrialBalance.php
+++ CRM/Report/Form/Contribute/TrialBalance.php
@@ -37,13 +37,6 @@ class CRM_Report_Form_Contribute_TrialBalance extends CRM_Report_Form {
   /**
    */
   public function __construct() {
-    $params['labelColumn'] = 'name';
-    $financialAccountType = CRM_Core_PseudoConstant::get('CRM_Financial_DAO_FinancialAccount', 'financial_account_type_id', $params);
-    $financialBalanceField = 'opening_balance';
-    if (CRM_Contribute_BAO_Contribution::checkContributeSettings('prior_financial_period')) {
-      $financialBalanceField = 'current_period_opening_balance';
-    }
-
     $this->_columns = array(
       'civicrm_financial_account' => array(
         'dao' => 'CRM_Financial_DAO_FinancialAccount',
@@ -72,12 +65,12 @@ class CRM_Report_Form_Contribute_TrialBalance extends CRM_Report_Form {
           'debit' => array(
             'title' => ts('Debit'),
             'required' => TRUE,
-            'dbAlias' => 'SUM(debit) + IF (financial_account_type_id = ' . array_search('Asset', $financialAccountType) . ", {$financialBalanceField}, 0)",
+            'dbAlias' => 'SUM(debit)',
           ),
           'credit' => array(
             'title' => ts('Credit'),
             'required' => TRUE,
-            'dbAlias' => 'SUM(credit) + IF (financial_account_type_id = ' . array_search('Liability', $financialAccountType) . ", {$financialBalanceField}, 0)",
+            'dbAlias' => 'SUM(credit)',
           ),
         ),
       ),
@@ -92,8 +85,11 @@ class CRM_Report_Form_Contribute_TrialBalance extends CRM_Report_Form {
 
 
   public function from() {
-    $closingDate = 'now()';
     $priorDate = CRM_Contribute_BAO_Contribution::checkContributeSettings('prior_financial_period');
+    $financialBalanceField = 'opening_balance';
+    if ($priorDate) {
+      $financialBalanceField = 'current_period_opening_balance';
+    }
     if (empty($priorDate)) {
       $date = new DateTime();
       $date->modify("last day of previous month");
@@ -105,18 +101,24 @@ class CRM_Report_Form_Contribute_TrialBalance extends CRM_Report_Form {
       $closingDate = date('Y-m-t', strtotime($priorDate));
       $where = " BETWEEN DATE('$priorDate') AND DATE('$closingDate') ";
     }
+    $params['labelColumn'] = 'name';
+    $financialAccountType = CRM_Core_PseudoConstant::get('CRM_Financial_DAO_FinancialAccount', 'financial_account_type_id', $params);
+    $financialAccountTypes = array(
+      array_search('Liability', $financialAccountType),
+      array_search('Asset', $financialAccountType)
+    );
     $this->_from = "
       FROM (
-        SELECT cft1.id, 0 AS credit, cft1.total_amount AS debit,
+        SELECT cft1.id, 0 as fid, 0 AS credit, cft1.total_amount AS debit,
           cft1.to_financial_account_id AS financial_account_id
           FROM civicrm_financial_trxn cft1
           WHERE cft1.trxn_date {$where}
         UNION
-        SELECT cft2.id, cft2.total_amount AS credit, 0 AS debit, cft2.from_financial_account_id
+        SELECT cft2.id, 0 as fid, cft2.total_amount AS credit, 0 AS debit, cft2.from_financial_account_id
           FROM civicrm_financial_trxn cft2
           WHERE cft2.trxn_date {$where}
         UNION
-        SELECT cft3.id, 0 AS credit, cfi3.amount AS debit, cfi3.financial_account_id
+        SELECT cft3.id, cfi3.id, 0 AS credit, cfi3.amount AS debit, cfi3.financial_account_id
           FROM civicrm_financial_item cfi3
             INNER JOIN civicrm_entity_financial_trxn ceft3 ON cfi3.id = ceft3.entity_id
               AND ceft3.entity_table = 'civicrm_financial_item'
@@ -124,13 +126,17 @@ class CRM_Report_Form_Contribute_TrialBalance extends CRM_Report_Form {
               AND cft3.to_financial_account_id IS NULL
           WHERE cfi3.transaction_date {$where}
         UNION
-        SELECT cft4.id, cfi4.amount AS credit, 0 AS debit, cfi4.financial_account_id
+        SELECT cft4.id, cfi4.id, cfi4.amount AS credit, 0 AS debit, cfi4.financial_account_id
           FROM civicrm_financial_item cfi4
           INNER JOIN civicrm_entity_financial_trxn ceft4 ON cfi4.id=ceft4.entity_id
             AND ceft4.entity_table='civicrm_financial_item'
           INNER JOIN civicrm_financial_trxn cft4 ON ceft4.financial_trxn_id=cft4.id
             AND cft4.from_financial_account_id IS NULL
           WHERE cfi4.transaction_date {$where}
+        UNION
+        SELECT 0 as tid, 0 as fid, IF (financial_account_type_id = " . array_search('Liability', $financialAccountType) . ", {$financialBalanceField}, 0) AS credit, IF (financial_account_type_id = " . array_search('Asset', $financialAccountType) . ", {$financialBalanceField}, 0) AS debit, cfa5.id
+          FROM civicrm_financial_account cfa5
+          WHERE cfa5.financial_account_type_id IN (" . implode(', ', $financialAccountTypes) . ")
 
       ) AS {$this->_aliases['civicrm_financial_trxn']}
       INNER JOIN civicrm_financial_account {$this->_aliases['civicrm_financial_account']} ON {$this->_aliases['civicrm_financial_trxn']}.financial_account_id = {$this->_aliases['civicrm_financial_account']}.id
