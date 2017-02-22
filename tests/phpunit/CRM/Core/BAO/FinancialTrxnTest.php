ec1a48a3a0360922f6f3fbbea69988a6f3d393f5
Pradeep Nayak <pradpnayak@gmail.com>
2017-02-23 04:43:44 +0530

    CRM-20158, added test
    ----------------------------------------
    * CRM-20158: Store card type and last 4 digits of credit card
      https://issues.civicrm.org/jira/browse/CRM-20158
    
    Conflicts:
    
    	tests/phpunit/CRM/Core/BAO/FinancialTrxnTest.php

--- tests/phpunit/CRM/Core/BAO/FinancialTrxnTest.php
+++ tests/phpunit/CRM/Core/BAO/FinancialTrxnTest.php
@@ -265,4 +265,25 @@ class CRM_Core_BAO_FinancialTrxnTest extends CiviUnitTestCase {
     $this->checkArrayEquals($params, $expectedResult);
   }
 
+  /**
+   * Test for hasPaymentProcessorTrxn().
+   */
+  public function testHasPaymentProcessorTrxn() {
+    $cid = $this->individualCreate();
+    $paymentProcessorID = $this->processorCreate();
+    $params = array(
+      'contact_id' => $cid,
+      'receive_date' => '2016-01-20',
+      'total_amount' => 100,
+      'financial_type_id' => 1,
+    );
+    $contribution = CRM_Contribute_BAO_Contribution::create($params);
+    $paymentProcessorId = CRM_Core_BAO_FinancialTrxn::hasPaymentProcessorTrxn($contribution->id);
+    $this->assertEquals($paymentProcessorId, NULL);
+    $params['payment_processor'] = $paymentProcessorID;
+    $contribution = CRM_Contribute_BAO_Contribution::create($params);
+    $paymentProcessorId = CRM_Core_BAO_FinancialTrxn::hasPaymentProcessorTrxn($contribution->id);
+    $this->assertEquals($paymentProcessorId, $paymentProcessorID);
+  }
+
 }
