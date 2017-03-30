1368c1b76158ca75a350128bb2f011d85119c3db
Edsel <edsel.lopez@jmaconsulting.biz>
2017-03-30 15:56:09 +0530

    IIDA-119 Tweaks to deferred revenue report

--- CRM/Financial/BAO/FinancialAccount.php
+++ CRM/Financial/BAO/FinancialAccount.php
@@ -439,8 +439,8 @@ LIMIT 1";
    * @return array of Deferred Financial Account
    *
    */
-  public static function getAllDeferredFinancialAccount() {
-    $query = "SELECT cfa.id, cfa.name FROM civicrm_entity_financial_account ce
+  public static function getAllDeferredFinancialAccount($includeCode = FALSE) {
+    $query = "SELECT cfa.id, cfa.name, cfa.accounting_code FROM civicrm_entity_financial_account ce
 INNER JOIN civicrm_financial_account cfa ON ce.financial_account_id = cfa.id
 WHERE `entity_table` = 'civicrm_financial_type' AND cfa.is_active = 1 AND ce.account_relationship = %1 GROUP BY cfa.id";
     $deferredAccountRel = key(CRM_Core_PseudoConstant::accountOptionValues('account_relationship', NULL, " AND v.name LIKE 'Deferred Revenue Account is' "));
@@ -448,7 +448,11 @@ WHERE `entity_table` = 'civicrm_financial_type' AND cfa.is_active = 1 AND ce.acc
     $dao = CRM_Core_DAO::executeQuery($query, $queryParams);
     $financialAccount = array();
     while ($dao->fetch()) {
-      $financialAccount[$dao->id] = $dao->name;
+      $label = $dao->name;
+      if ($includeCode) {
+        $label .= " ({$dao->accounting_code})" ;
+      }
+      $financialAccount[$dao->id] = $label;
     }
     return $financialAccount;
   }
